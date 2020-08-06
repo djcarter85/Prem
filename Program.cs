a270e700d77cec8e80d4db69fb74ff0256265754
Dan Carter <djcarter85@gmail.com>
2020-08-06 08:58:50 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
